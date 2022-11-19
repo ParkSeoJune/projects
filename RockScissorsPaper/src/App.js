0bb15a8d421a86b409faab659d109db4d650da5a
ParkSeoJune <tjwns4122@gmail.com>
2022-11-20 01:12:09 +0900

    1120

--- RockScissorsPaper/src/App.js
+++ RockScissorsPaper/src/App.js
@@ -1,48 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-
-
 function App() {
   const [imoji, setImogi] = useState('✊');
   const [num, setNum] = useState(0);
   const [user, setUser] = useState(0);
-  const [result, setResult] = useState();
-  // useEffect(()=> {
-  //   changeNum.current = callback();
-  // })
-//////////// 문제 렌더링보다 num값이  바뀌는게 빠름 그리고 result에 음수가 안들어가는것 같음
+  const [result, setResult] = useState(0);
+  const [rere, setRe] = useState('');
+
   useEffect(()=> { 
     const timer = setInterval(() => { 
+      
+
+      setNum(num !== 3 ? num + 1 : 1);
       if(num===2) setImogi('✌️');
       else if(num===3) setImogi('🖐');
       else if(num===1) setImogi('✊');  
-      setNum(num !== 3 ? num + 1 : 1);
-    }, 350);
-    
-    if(user!==0) {
+      
+      if(user!==0) {
         clearInterval(timer);
-        console.log(`시스템값 : ${num}`);
-        console.log(`사용자값 : ${user}`)
-        setResult(result + (num - user));
-        console.log(result);
-
-        if(result===0) alert('무승부');
-        else if(result === 1 || result === -2){
-          alert('승!');
-        } // 3 - 2 유저(가위) 승, 2 - 1 유저(주먹) 승, 1 - 3 유저(보) 승
-        else if(result === -1 || result === 2) {
-          alert('패배..');
-        } // 1 - 2(가위) 패배, 2 - 3(보) 패배, 3 - 1(주먹) 패배
-    }
+        makeResult()
+;      } 
+    }, 350);
 
     return () => {
       clearInterval(timer);
     };
     
   }, [num]);
-  
-  
+
+  function makeResult() {
+    setResult(num - user);
+
+    console.log(`시스템값 : ${num}`);
+    console.log(`사용자값 : ${user}`); 
+  }
+
+  useEffect(()=> {
+    console.log(result);
+
+    if(result===0) setRe('무승부');
+    else if(result === 1 || result === -2){
+      setRe('승');
+    } // 3 - 2 유저(가위) 승, 2 - 1 유저(주먹) 승, 1 - 3 유저(보) 승
+    else if(result === -1 || result === 2) {
+      setRe('패배');
+    } // 1 - 2(가위) 패배, 2 - 3(보) 패배, 3 - 1(주먹) 패배
+
+    makeAlert();
+  }, [result]);
+
+  function makeAlert() {
+    alert(rere);
+    setUser(0);
+    setNum(0);
+  }
 
   return (
     <div className='compo'>
