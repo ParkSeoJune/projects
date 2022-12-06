0cd46c92cb0840e4f05ba63cf2e354054509db93
ParkSeoJune <tjwns4122@gmail.com>
2022-12-06 10:09:25 +0900

    401 에러 수정

--- search_github_profile/src/pages/Search.js
+++ search_github_profile/src/pages/Search.js
@@ -1,34 +1,20 @@
 import axios from "axios";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { Background, Input, Logo, Box } from "../Components/H";
-import Result from "./Result";
 
 const Search = () => {
   const navigate = useNavigate();
   const [value, setValue] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [followers, setFo] = useState();
-  const [image, setImage] = useState("");
   const onChange = (e) => setValue(e.target.value);
   const onSubmit = (e) => {
     e.preventDefault();
     // 값 넘겨주기
     getUser(value);
-    navigate("/profile", {
-      state: {
-        name: name,
-        description: description,
-        followers: followers,
-        image: image,
-      },
-    });
   };
 
   const getUser = async (username) => {
-    const token = "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";
+    const token = "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";
     const config = {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -39,10 +25,15 @@ const Search = () => {
         `https://api.github.com/users/` + username,
         config
       );
-      setName(response.data.name);
-      setDescription(response.data.bio);
-      setFo(response.data.followers);
-      setImage(response.data.avatar_url);
+
+      navigate("/profile", {
+        state: {
+          name: response.data.name,
+          description: response.data.bio,
+          followers: response.data.followers,
+          image: response.data.avatar_url,
+        },
+      });
     } catch (e) {
       throw new Error(`에러다`);
     }
