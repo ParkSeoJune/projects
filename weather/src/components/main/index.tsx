32abd07c095488691ad87fe87e15dd5330120852
ParkSeoJune <tjwns4122@gmail.com>
2023-05-03 14:20:15 +0900

    add: get weather api

--- weather/src/components/main/index.tsx
+++ weather/src/components/main/index.tsx
@@ -1,17 +1,38 @@
 import useGeolocation from "../../hooks/useGeolocation";
+import { REACT_APP_WEATHER_API } from "../../shared/config";
 import CenterAlignmentLayout from "../common/layout/justify/center";
 import * as S from "./style";
+import { useEffect, useState } from "react";
 
 function Main() {
   const location = useGeolocation();
+  const [weather, setWeather] = useState<any>();
+
+  const getWeatherData = async (lat: number, lng: number) => {
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${REACT_APP_WEATHER_API}`
+      );
+      const data = await response.json();
+      setWeather(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (location.loaded) {
+      if (location.coordinates) {
+        getWeatherData(location.coordinates.lat, location.coordinates.lng);
+      }
+    }
+  }, [location]);
 
   return (
     <>
       <CenterAlignmentLayout>
         <S.MainFrame>
-          {location.loaded
-            ? JSON.stringify(location)
-            : "Location data not available yet."}
           <S.Title>
             현재 내 위치의
             <br />
