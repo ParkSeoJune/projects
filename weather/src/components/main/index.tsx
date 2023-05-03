f35912369ff7b7d72309f531b0ddc5c422927fa0
ParkSeoJune <tjwns4122@gmail.com>
2023-05-03 21:04:00 +0900

    add: weather api

--- weather/src/components/main/index.tsx
+++ weather/src/components/main/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import useGeolocation from "../../hooks/useGeolocation";
 import { REACT_APP_WEATHER_API } from "../../shared/config";
 import CenterAlignmentLayout from "../common/layout/justify/center";
@@ -6,16 +7,22 @@ import { useEffect, useState } from "react";
 
 function Main() {
   const location = useGeolocation();
-  const [weather, setWeather] = useState<any>();
+  const [temp, setTemp] = useState<number>(0);
+  const [icon, setIcon] = useState<string>("");
+  const [wind, setWind] = useState<number>(0);
 
   const getWeatherData = async (lat: number, lng: number) => {
     try {
-      const response = await fetch(
+      const response: any = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${REACT_APP_WEATHER_API}`
       );
-      const data = await response.json();
-      setWeather(data);
-      console.log(data);
+      setTemp(Math.ceil(response.data.main.temp - 273.15));
+      setIcon(
+        "http://openweathermap.org/img/w/" +
+          response.data.weather[0].icon +
+          ".png"
+      );
+      setWind(response.data.wind.speed);
     } catch (error) {
       console.log(error);
     }
@@ -38,9 +45,14 @@ function Main() {
             <br />
             날씨는
           </S.Title>
-          <S.Temperature>27°C</S.Temperature>
-          <S.Description color="#F2C0C0">
-            따뜻하고 바람이 안부는 날이네요!
+          <S.Icon src={icon} />
+          <S.Temperature>{temp}°C</S.Temperature>
+          <S.Description color={temp > 25 ? "#F2C0C0" : "#C0CEF2"}>
+            {temp > 25 ? (
+              <>오늘은 따뜻하고 바람이 {wind}m/s만큼 불어요</>
+            ) : (
+              <>오늘은 쌀쌀하고 바람이 {wind}m/s만큼 불어요</>
+            )}
           </S.Description>
         </S.MainFrame>
       </CenterAlignmentLayout>
