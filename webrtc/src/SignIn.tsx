e32736cc452cb85f001ce5ef6ae4246e726d39a9
ParkSeoJune <tjwns4122@gmail.com>
2023-05-08 19:46:46 +0900

    add: sign in with google

---
+++ webrtc/src/SignIn.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import * as S from "./style";
+import { auth } from "./firebase";
+import {
+  GoogleAuthProvider,
+  UserCredential,
+  signInWithPopup,
+} from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+function SignIn() {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    const provider = new GoogleAuthProvider();
+    provider.addScope("profile");
+    provider.addScope("email");
+
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user: UserCredential["user"] = result.user;
+
+        auth.onAuthStateChanged((user) => {
+          if (user) {
+            console.log(user);
+            navigate("/chatting", {
+              state: {
+                uid: user.uid,
+              },
+            });
+          }
+        });
+      })
+      .catch((error: any) => {
+        Error(error);
+      });
+  };
+
+  return (
+    <>
+      <S.App>
+        <>
+          <h1>Sign in with Google 🔥</h1>
+          <S.SignInButton onClick={onClick}>Sign in with Google</S.SignInButton>
+        </>
+      </S.App>
+    </>
+  );
+}
+
+export default SignIn;
---
+++ webrtc/src/style.ts
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+
+export const App = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignInButton = styled.button`
+  border-style: none;
+  border-radius: 12px;
+  width: 160px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #77d6b3;
+  color: #ffffff;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export const CreateButton = styled.button`
+  border-style: none;
+  border-radius: 12px;
+  width: 160px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #77d6b3;
+  color: #ffffff;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export const EnterButton = styled.button`
+  border-style: none;
+  border-radius: 12px;
+  width: 160px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #7848de;
+  color: #ffffff;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export const Text = styled.p`
+  font-size: 22px;
+  font-weight: 600;
+  color: #000000;
+  margin: 0px;
+`;
