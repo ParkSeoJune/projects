499611202f6c048f10a067bbb43804c6d56a9b8e
ParkSeoJune <tjwns4122@gmail.com>
2023-05-15 09:11:34 +0900

    add: chatroom page publishing

--- webrtc/src/ChatRoom/ChatRoom.tsx
+++ webrtc/src/ChatRoom/ChatRoom.tsx
@@ -1,11 +1,46 @@
+import { useLocation } from "react-router-dom";
 import { App } from "../style";
 import * as S from "./style";
+import { useEffect, useState } from "react";
+
+interface UserType {
+  name: string;
+  profileImageUrl: string;
+  uid: string;
+  user1: boolean;
+}
 
 function ChatRoom() {
+  const location = useLocation();
+  const [user1, setUser1] = useState<UserType>();
+  const [user2, setUser2] = useState<UserType>();
+
+  useEffect(() => {
+    if (location.state.user1) {
+      setUser1(location.state);
+    } else {
+      setUser2(location.state);
+    }
+  }, []);
+
   return (
     <>
       <App>
-        <S.Frame></S.Frame>
+        <S.Frame>
+          <S.Top>
+            <S.User>
+              <S.Profile src={user1?.profileImageUrl} />
+              <S.UserName>{user1?.name}</S.UserName>
+              <S.Profile src={user2?.profileImageUrl} />
+              <S.UserName>{user2?.name}</S.UserName>
+            </S.User>
+          </S.Top>
+          <S.ChatFrame>
+            <S.Face />
+            <S.Face />
+          </S.ChatFrame>
+          <S.Disconnect>통화 종료</S.Disconnect>
+        </S.Frame>
       </App>
     </>
   );
--- webrtc/src/ChatRoom/style.ts
+++ webrtc/src/ChatRoom/style.ts
@@ -1,9 +1,66 @@
 import styled from "@emotion/styled";
 
 export const Frame = styled.main`
-  width: 700px;
+  width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
+  align-items: center;
+`;
+
+export const Top = styled.section`
+  width: 700px;
+  height: 140px;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`;
+
+export const User = styled.div`
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Profile = styled.img`
+  width: 80px;
+  height: 80px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 50%;
+`;
+
+export const UserName = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+  color: #000000;
+  margin-top: 20px;
+`;
+
+export const ChatFrame = styled.section`
+  width: 100vw;
+  height: 600px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Face = styled.div`
+  width: 49.5vw;
+  height: 600px;
   background-color: #000000;
+  border-radius: 14px;
+`;
+
+export const Disconnect = styled.button`
+  width: 150px;
+  height: 50px;
+  text-align: center;
+  background-color: #f2c0c0;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: 600;
+  border-style: none;
+  border-radius: 12px;
+  margin-top: 50px;
 `;
